83e73ee7844917780f97b7612c1b45629654b084
Angelo7x <abennici96@gmail.com>
2021-10-19 16:51:23 +0200

    snack 2 completato

--- snack2/index.php
+++ snack2/index.php
@@ -1,28 +1,19 @@
-<?php
+<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
+che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok
+ stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
-$calendarioPartite = [
-    [
-        "squadraCasa" => "Brindisi",
-        "squadraOspite" => "fortitudoBologna",
-        "puntiCasa" => 105,
-        "puntiOspite" => 93,
-    ],
-    [
-        "squadraCasa" => "Reggiana",
-        "squadraOspite" => "Trento",
-        "puntiCasa" => 74,
-        "puntiOspite" => 78,
-    ],
-    [
-        "squadraCasa" => "Bescia",
-        "squadraOspite" => "Napoli",
-        "puntiCasa" => 98,
-        "puntiOspite" => 88,
-    ],
-    [
-        "squadraCasa" => "Cremona",
-        "squadraOspite" => "Varese",
-        "puntiCasa" => 94,
-        "puntiOspite" => 78,
-    ]
-];
+
+<?php
+$name = $_GET["name"];
+$mail = $_GET["mail"];
+$age = $_GET["age"];
+
+if (strlen($name) > 3 && (strpos($mail, "@") && strpos($mail, ".") ) && (is_numeric($age) == true) ) {
+    
+    echo "Accesso consentito ";
+
+} else {
+    
+    echo "Accesso negato";
+}
+
